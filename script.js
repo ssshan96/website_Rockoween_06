4ee6fb2601e905df1b7ecad56af41e7adb0769b5
ssshan96 <78926459+ssshan96@users.noreply.github.com>
2023-10-16 17:35:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const card = document.getElementById('card');
+    const tapMe = document.getElementById('tap-me');
     const hyperlink = document.getElementById('hyperlink');
   
     card.addEventListener('click', function() {
-      this.style.transform = this.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+      const isFlipped = this.style.transform === 'rotateY(180deg)';
+      this.style.transform = isFlipped ? 'rotateY(0deg)' : 'rotateY(180deg)';
+  
+      if (isFlipped) {
+        tapMe.style.animation = 'bounce 2s infinite';  // Resume animation
+      } else {
+        tapMe.style.animation = 'none';  // Stop animation
+      }
     });
   
-    // Prevent the card from flipping when hyperlink is clicked
     hyperlink.addEventListener('click', function(event) {
       event.stopPropagation();
     });
